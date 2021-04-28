feed0dc75f62ec33399b138fb108e6033d886ef9
karlthomaas <73159568+karlthomaas@users.noreply.github.com>
2021-04-28 18:29:15 +0300

    Finished all scenarios.

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ def lesson_pick(lesson):
 def course_pick(course):
     driver.find_element_by_xpath(f'//body/div[3]/div[4]/div[1]/div[1]/div[1]/div[2]/a[{course}]/div[2]').click()
 
+def console_log(sentence):
+    sentence_length = len(sentence)
+    print('-' *sentence_length)
+    print(sentence)
+    print('-' *sentence_length)
 
 memrise_login()
 time.sleep(5)
@@ -45,7 +50,7 @@ time.sleep(5)
 lesson_pick('S1 Insight Upper intermediate')
 time.sleep(5)
 # course number
-course_pick('6')
+course_pick('5')
 
 time.sleep(5)
 # current url ->
@@ -58,6 +63,17 @@ try:
 except Exception as e:
     print('Error:')
     print(e)
+
+def word_check(word):
+    """ Returns the value of key and key of value"""
+    if word in words_dictionary.keys():
+        return words_dictionary[word]
+
+    elif word in words_dictionary.values():
+        for key, value in words_dictionary.items():
+            if value == word:
+                return key
+
 time.sleep(5)
 driver.find_element_by_xpath('//a[contains(text(),\'Continue learning\')]').click()
 time.sleep(5)
@@ -67,31 +83,78 @@ Next line types the category header ->
 PS! Need to split the header and take in the first sentence (header)
 """
 state = True
+
+answering_cooldown = 2
 while state:
-    category = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]").text
-    if category == 'Type the correct translation':
-        """Selenium only need to type the correct translation into input box and click enter"""
+    try:
+        category = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]").text
+        if category == 'Type the correct translation':
+            """Selenium only need to type the correct translation into input box and click enter"""
 
-        print('DETECTED: Type the correct translation')
+            print('DETECTED: Type the correct translation')
 
-        # takes the word
-        aWord = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]").text
-        # takes the word translation
-        bWord = words_dictionary[aWord]
-        # selects the input box
-        input_box = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[2]/div[1]/input[1]")
-        # sends the answer into input box
-        input_box.send_keys(bWord)
+            # takes the word
+            aWord = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]").text
+            # takes the word translation
+            bWord = words_dictionary[aWord]
+            # selects the input box
+            input_box = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[2]/div[1]/input[1]")
+            # sends the answer into input box
+            input_box.send_keys(bWord)
 
-    elif category == 'Choose the correct translation':
-        print('DETECTED: Choose the correct translation')
+        elif category == 'Choose the correct translation':
+            print('DETECTED: Choose the correct translation')
 
-    else:
-        """ There's nothing to do with Lesson card, script will skip it"""
+            # otsitav sõna ->
+            searched_word = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]").text
 
-        print('DETECTED: Lesson card')
-        print('Skipping.')
-        # Clicks the Next button ->
-        driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/button[1]").click()
+            # Prints out the valikvastused ->
+            ctct1 = driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[2]").text
 
-    time.sleep(5)
+            if ctct1[0] == '1':
+                list = ctct1.split('\n')
+                list.insert(0, '')
+                answer = word_check(searched_word)
+                print(f'SEARCHED WORD: {searched_word}\nANSWER: {answer}')
+                choice_answer = list.index(answer) - 1
+
+                site = driver.find_element_by_xpath('//html')
+                site.send_keys(list[choice_answer])
+            else:
+                site = driver.find_element_by_xpath('//html')
+                # valiksõnad input boxi all
+                list = ctct1.split('\n')
+                list.insert(0, '')
+
+                answer = word_check(searched_word)
+                # vastus tehtud eraldi juppideks
+                answer_splitted = answer.split(' ')
+
+                # activates the number answering
+                site.send_keys('1')
+                # nii mitu korda käib, kuniks sõna on täiesti läbi
+                for i in range(len(answer_splitted)):
+
+                    number = list.index(answer_splitted[i])
+                    site.send_keys(str(number))
+                    time.sleep(1)
+                print(f'SEARCHED WORD: {searched_word}\nANSWER: {answer}')
+
+
+
+        else:
+            """ There's nothing to do with Lesson card, script will skip it"""
+
+            print('DETECTED: Lesson card')
+            print('Skipping.')
+            # Clicks the Next button ->
+            driver.find_element_by_xpath("//body/div[@id='__next']/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[5]/button[1]").click()
+
+        time.sleep(answering_cooldown)
+    except Exception as e:
+        print(e)
+        print('Ran out of words. Restarting!')
+        driver.get(current_url)
+        time.sleep(2)
+        driver.find_element_by_xpath('//a[contains(text(),\'Continue learning\')]').click()
+        time.sleep(2)
